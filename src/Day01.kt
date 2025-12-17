984048cb4fb2cc3af28a5dc03dae28210893f659
Faisal rasak <monsterkillu@gmail.com>
2025-12-17 10:01:45 +0530

    Implement Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,96 @@
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val sample = "day01sample"
+    val actual = "day01"
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    val lines = readInput(actual)
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    //part1(lines)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    // PART 2
+    part2(lines)
 }
+
+fun part2(lines: List<String>) {
+    var dialAt = 50
+    var numZeros = 0
+    var numZeroCrossings = 0
+
+    fun rotateDialLeft(rotations: Int) {
+        repeat(rotations) {
+            if (dialAt == 0) {
+                numZeroCrossings += 1
+            }
+            dialAt -= 1
+            // reset to 99
+            if (dialAt<0) {
+                dialAt = 99
+            }
+        }
+    }
+
+    fun rotateDialRight(rotations: Int) {
+        repeat(rotations) {
+            if (dialAt == 0) {
+                numZeroCrossings += 1
+            }
+            dialAt += 1
+            // reset to 0
+            if (dialAt>99) {
+                dialAt = 0
+            }
+        }
+    }
+
+    lines.forEach {
+        numZeroCrossings = 0
+        val direction = it[0]
+        val rotations = it.drop(1).toInt()
+
+        when (direction) {
+            'L' -> rotateDialLeft(rotations)
+            'R' -> rotateDialRight(rotations)
+        }
+
+        println("The dial is rotated $it to point at $dialAt. ($numZeroCrossings zeros crossed)")
+        if (numZeroCrossings>0) {
+            numZeros += numZeroCrossings
+        }
+    }
+
+    println("part 2: numzeros = $numZeros")
+}
+
+
+
+fun part1(lines: List<String>) {
+    var pointingVal = 50
+    var numZeros = 0
+    println("The dial starts by pointing at $pointingVal")
+    lines.forEach {
+        val direction = it[0]
+        //println(it)
+        if (direction == 'L') {
+            pointingVal -= it.drop(1).toInt()
+            pointingVal = pointingVal.zeroCorrection()
+            println("The dial is rotated $it to point at $pointingVal.")
+        } else if (direction == 'R') {
+            pointingVal += it.drop(1).toInt()
+            pointingVal = pointingVal.zeroCorrection()
+            println("The dial is rotated $it to point at $pointingVal.")
+        }
+
+        if (pointingVal == 0) numZeros++
+    }
+    println("part 1 - numzeros = $numZeros")
+}
+
+private fun Int.zeroCorrection(): Int {
+    val correctedValue = this%100
+    return if (correctedValue < 0) {
+        100 + correctedValue
+    } else if (correctedValue > 99){
+        correctedValue - 100
+    } else correctedValue
+}
+
